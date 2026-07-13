b3324b86d29fbd36042c1b0b544a1f7346129e95
youranwang-hub <youranwang636@gmail.com>
2026-07-13 15:06:22 +0800

    完成实验4（红外接收实验）

---
+++ wangyouran/实验4_IR红外接收实验/BSP/usart.h
@@ -0,0 +1,8 @@
+#ifndef __USART_H__
+#define __USART_H__
+
+#include "stm32f4xx.h"
+
+void UART2_Init(u32 bound);
+
+#endif
---
+++ wangyouran/实验4_IR红外接收实验/USER/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "stm32f4xx.h"
+#include "ir_recv.h"
+#include "usart.h"
+
+int main(void)
+{
+	UART2_Init(115200);
+	IR_Recv_Init();
+
+	printf("IRRecv Init OK\r\n");
+
+	while(1)
+	{
+		IR_Recv();
+	}
+}
