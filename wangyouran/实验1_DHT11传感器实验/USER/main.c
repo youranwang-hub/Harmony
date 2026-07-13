23083cedf091addc2bec19b739765bdad050179e
youranwang-hub <youranwang636@gmail.com>
2026-07-13 12:02:52 +0800

    完成实验1温度传感器实验以及README文档

---
+++ wangyouran/实验1_DHT11传感器实验/USER/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "stm32f4xx.h"
+#include "dht11.h"
+#include "usart.h"
+#include "delay.h" //简单延时
+
+
+
+/**
+* @brief 主函数
+* @param 无
+* @retval 无
+*/
+int main( void ){
+DHT11_Data Data;
+
+UART2_Init(115200);
+DHT11_Init();
+printf("DHT11 Init OK\r\n");
+
+delay_ms(1000);
+
+while(1){
+if(DHT11_ReadData( & Data ) == 0){
+printf("HUMI:%d.%d_TEMP:%d.%d\r\n",\
+Data.humi_int,Data.humi_deci,Data.temp_int,Data.temp_deci);
+}
+else {
+printf("Read DHT11 ERROR!\r\n");
+}
+delay_ms(10000);
+}
+}
+
+
+
+
+
