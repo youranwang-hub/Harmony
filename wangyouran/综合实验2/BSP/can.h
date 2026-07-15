fdccada4f8ae49403dd9050d4fd9b08d272aab06
youranwang-hub <youranwang636@gmail.com>
2026-07-15 14:40:22 +0800

    refactor: 移除menu_settime功能及综合实验2的BSP模块

--- wangyouran/综合实验2/BSP/can.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CAN_H__
-#define __CAN_H__
-#include "stm32f4xx.h"
-
-void CAN1_Init(void);
-void CAN1_SendMsg(uint32_t uID, uint8_t *pData, uint32_t uLen);
-
-#endif
--- wangyouran/综合实验2/BSP/rtc.h
+++
@@ -1,8 +0,0 @@
-#ifndef __RTC_H__
-#define __RTC_H__
-
-#include "stm32f4xx.h"
-
-void BSP_RTC_Init(void);
-
-#endif
